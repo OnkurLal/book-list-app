6a5e26fc74f918ba2cbcd5f18180a08d04b8decf
Onkur Lal <onkur24lal@gmail.com>
2022-11-29 21:31:09 -0500

    updated books using mongoDB database

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const router = require('./routes/index-route')
-const welcome = require('./module-practice.js')
-const bookData = require('./book-data')
-app.set('views', './views');
-app.set('view engine', 'pug');
+const router = require("./routes/index-route");
+const welcome = require("./module-practice.js");
+const bookData = require("./book-data");
+const mongoose = require("mongoose");
+const Book = require("./models/Book");
+app.set("views", "./views");
+app.set("view engine", "pug");
 app.use(express.json());
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/', router);
-console.log(welcome('Onkur'));
+app.use("/", router);
+console.log(welcome("Onkur"));
 
-
-
-app.listen(3001)
+app.listen(3001);
--- routes/index-route.js
+++ routes/index-route.js
@@ -1,45 +1,74 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bookData = require('../book-data')
+const bookData = require("../book-data");
+const Book = require("../models/Book");
+const mongoose = require("mongoose");
+const { response } = require("express");
+mongoose.connect(
+  `mongodb+srv://onkur24lal:baller24@cluster0.wb2m06w.mongodb.net/?retryWrites=true&w=majority`
+);
 
-// router.get('/games/:id', (req, resp)=> {
-//     resp.send(`Get Route on router ${req.params.id}`)
-// })
-// router.get('/', (req, resp) => {
-//     resp.send('Get Route for home page')
-// })
-// router.post('/:games/id', (req, resp) => {
-//     resp.send(`3rd route ${req.params.games}`)
-// })
+router.get("/", async (req, resp) => {
+  const books = await Book.find();
+  resp.send(books);
+});
+router.get("/update/:id", async (req, resp) => {
+  const book = await Book.find({ isbn: req.params.id });
+  resp.render("index", {
+    isbn: book[0].isbn,
+    title: book[0].title,
+    subtitle: book[0].subtitle,
+    author: book[0].author,
+    published: book[0].published,
+    publisher: book[0].publisher,
+    pages: book[0].pages,
+    description: book[0].description,
+    website: book[0].website,
+  });
+});
+router.post("/update/:id", async (req, resp) => {
+  await Book.findOneAndUpdate({ isbn: req.params.id }, req.body);
+  resp.redirect(`/${req.params.id}`);
+});
+router.get("/delete/:id", async (req, resp) => {
+  const book = await Book.find({ isbn: req.params.id });
+  resp.render("delete", {
+    isbn: book[0].isbn,
+    title: book[0].title,
+    subtitle: book[0].subtitle,
+    author: book[0].author,
+    published: book[0].published,
+    publisher: book[0].publisher,
+    pages: book[0].pages,
+    description: book[0].description,
+    website: book[0].website,
+  });
+});
+router.post("/delete/:id", async (req, resp) => {
+  await Book.deleteOne({ isbn: req.params.id });
+  resp.redirect("/");
+});
 
-router.get('/', (req, resp) => {
-    resp.send(bookData.books)
-})
-router.get('/about', (req, resp) => {
-    resp.send(bookData.books)
-})
-router.get('/update/:id', (req, resp) => {
-    const book = bookData.books.filter((book) => book.isbn === req.params.id);
-    resp.render('index', {isbn: book[0].isbn, title: book[0].title, subtitle: book[0].subtitle, author: book[0].author, published: book[0].published, publisher: book[0].publisher, pages: book[0].pages, description: book[0].description, website: book[0].website})
-})
-router.delete('/:id', (req, resp) => {
-    resp.send(`Book ${req.params.id} has been successfully deleted!`);
-})
-router.get('/:id', (req, resp) => {
-    resp.send(bookData.books.filter((book) => book.isbn === req.params.id))
-})
-router.post('/update/:id', async (req, resp) => {
-    const updatedBook = await bookData.books.filter((book)=> book.isbn === req.params.id)
-    updatedBook[0].isbn = req.body.isbn,
-    updatedBook[0].title = req.body.title,
-    updatedBook[0].subtitle = req.body.subtitle,
-    updatedBook[0].author = req.body.author,
-    updatedBook[0].published = req.body.published,
-    updatedBook[0].publisher = req.body.publisher,
-    updatedBook[0].pages = req.body.pages,
-    updatedBook[0].description = req.body.description,
-    updatedBook[0].website = req.body.website
-    
-    resp.redirect(`/${updatedBook[0].isbn}`)
-})
-module.exports = router
+router.get("/new", (req, resp) => {
+  resp.render("new");
+});
+router.post("/new", async (req, resp) => {
+  await Book.create(req.body);
+  resp.redirect("/");
+});
+
+router.get("/:id", async (req, resp) => {
+  const book = await Book.find({ isbn: req.params.id });
+  resp.render("view", {
+    isbn: book[0].isbn,
+    title: book[0].title,
+    subtitle: book[0].subtitle,
+    author: book[0].author,
+    published: book[0].published,
+    publisher: book[0].publisher,
+    pages: book[0].pages,
+    description: book[0].description,
+    website: book[0].website,
+  });
+});
+module.exports = router;
